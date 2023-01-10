495cfc791d2f48438d2b1c8a58695e573976b12c
prajjwals-stack <prajjwal.soni@quantrium.ai>
2023-01-10 22:10:16 +0530

    Database schema added

--- Backend/Model/BaseModel.py
+++ Backend/Model/BaseModel.py
@@ -1,2 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 
+
+class UserSchema(BaseModel):
+    email:str=Field(...)
+    username:str=Field(...)
+    hashed_password:str=Field(...)
+    mobile_number:str=Field(...)
+
+class PasswordSchema(BaseModel):
+    data:str=Field(...)
+    encrypted_password:str=Field(...)
+
+class PinSchema(BaseModel):
+    data:str=Field(...)
+    encrypted_pin:str=Field(...)
