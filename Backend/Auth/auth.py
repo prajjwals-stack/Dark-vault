823b7180ffa0b3000b9803722a0ac6aea96526bc
prajjwals-stack <prajjwal.soni@quantrium.ai>
2023-01-26 20:24:47 +0530

    Added:functionality to check if user already exist

--- Backend/Auth/auth.py
+++ Backend/Auth/auth.py
@@ -6,7 +6,13 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class Auth:
     def addUser(self,data:dict):
-        
+        if(self.checkUser(data['email'])):
+            return "user already exists"
         data['password']=pwd_context.encrypt(data['password'])
         db["user management"].insert_one(dict(data))
         return data
+
+    def checkUser(self,email:str)->bool:
+        if(db[USER_COLLECTION].find_one({'email':email})):
+            return True
+        return False
