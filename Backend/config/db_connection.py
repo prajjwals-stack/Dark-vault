91a60562418eaf9e2bfc99680232e8da5e1430b0
prajjwals-stack <prajjwal.soni@quantrium.ai>
2023-01-26 13:12:14 +0530

    Added: Local DB connection

--- Backend/config/db_connection.py
+++ Backend/config/db_connection.py
@@ -2,5 +2,7 @@ from pymongo import MongoClient
 # from config.config import MONGO_COULD_CLIENT,MONGO_CLOUD_DATABASE
 
 
-m_client = MongoClient("mongodb+srv://Prajjwal:Soni%402310@cluster0.ynjt4nh.mongodb.net/test")
+m_client = MongoClient("mongodb://localhost:27017")
 db = m_client["dark_vault"]
+
+db["user management"].insert_one({"test":"test"})
